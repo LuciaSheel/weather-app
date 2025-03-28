9fd247df3157434f63dbcfa1b9af4e647bf80269
Lucia <luciasheel@gmail.com>
2025-03-28 16:09:54 -0700

    wrap App component with ThemeProvider for context management

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 // Renders the app into the DOM
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from "./App";
+import { ThemeProvider } from "./context/ThemeContext"; // Import ThemeProvider
+import "./styles/global.css"; // Global styles
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+);

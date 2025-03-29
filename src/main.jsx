d13558008000bd65904f3e4bfd1afb85726eeedb
Lucia <luciasheel@gmail.com>
2025-03-29 13:25:02 -0700

    style: format import statements in main.jsx for consistency

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App";
-import { ThemeProvider } from "./context/ThemeContext"; // Import ThemeProvider
-import "./styles/global.css"; // Global styles
+import { ThemeProvider } from "./context/ThemeContext";
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

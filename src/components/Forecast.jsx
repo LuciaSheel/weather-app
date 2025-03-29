a497401a86160470553f33a3123873ddd2189417
Lucia <luciasheel@gmail.com>
2025-03-29 04:48:06 -0700

    add: implement Forecast component to display grouped weather data by day

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,1 +1,33 @@
 // Shows weather forecast
+const Forecast = ({ forecast }) => {
+  // Group forecast data by day
+  const groupedForecast = forecast.list.reduce((acc, item) => {
+    const date = item.dt_txt.split(" ")[0];
+    if (!acc[date]) {
+      acc[date] = [];
+    }
+    acc[date].push(item);
+    return acc;
+  }, {});
+
+  return (
+    <div className="forecast">
+      {Object.keys(groupedForecast).map((date) => (
+        <div key={date} className="forecast-day">
+          <h3>{date}</h3>
+          <div className="forecast-details">
+            {groupedForecast[date].map((item, index) => (
+              <div key={index}>
+                <p>{item.main.temp}°C</p>
+                <p>Humidity: {item.main.humidity}%</p>
+                <p>Wind Speed: {item.wind.speed} m/s</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Forecast;
